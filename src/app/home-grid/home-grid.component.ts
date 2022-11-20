2514d352d788de93f40c269126c0538f946f62e3
Priyanshi <ga.priyanshi@gmail.com>
2022-11-20 16:26:45 +0530

    asd

--- src/app/home-grid/home-grid.component.ts
+++ src/app/home-grid/home-grid.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ColDef } from 'ag-grid-community';
+// import { ColDef } from 'ag-grid-community';
 import { HomeService } from './home.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home-grid',
@@ -8,18 +9,24 @@ import { HomeService } from './home.service';
   styleUrls: ['./home-grid.component.less']
 })
 export class HomeGridComponent implements OnInit {
-  columnDefs: { headerName: string; field: string; filter: boolean; sortable: boolean; resizable: boolean; minWidth: number; }[];
+  columnDefs: { headerName: string; field: string; filter: boolean; sortable: boolean; resizable: boolean; minWidth: number; width: number;}[];
   gridApi: any;
   gridColumnApi: any;
   rowData: any = [];
+  profileForm = new FormGroup({
+    keyword: new FormControl('', Validators.required),
+    tweets: new FormControl('', Validators.required),
+  });
 
   constructor(private homeService: HomeService) { 
     this.columnDefs = [
-      {headerName:'keyword', field: 'keyword', filter: true, sortable: true, resizable: true, minWidth:300},
-      {headerName:'negative', field: 'negative', filter: true, sortable: true, resizable: true, minWidth:300},
-      {headerName:'neutral', field: 'neutral', filter: true, sortable: true, resizable: true, minWidth:300},
-      {headerName:'positive', field: 'positive', filter: true, sortable: true, resizable: true, minWidth:300},
-      // {headerName:'First Name', field: 'first_name', filter: true, sortable: true, resizable: true, minWidth:300},
+      // {headerName:'Keyword', field: 'keyword', filter: true, sortable: true, resizable: true, minWidth:300},
+      {headerName:'Tweets', field: 'tweets', filter: true, sortable: true, resizable: true, minWidth:300, width: 900},
+      {headerName:'Sentiments', field: 'sentiments', filter: true, sortable: true, resizable: true, minWidth:300, width: 300}
+      
+      // {headerName:'negative', field: 'negative', filter: true, sortable: true, resizable: true, minWidth:300},
+      // {headerName:'neutral', field: 'neutral', filter: true, sortable: true, resizable: true, minWidth:300},
+      // {headerName:'positive', field: 'positive', filter: true, sortable: true, resizable: true, minWidth:300},
     ]
   }
 
@@ -29,16 +36,32 @@ export class HomeGridComponent implements OnInit {
   onGridReady(params: any){
     this.gridApi = params.api;
     this.gridColumnApi = params.columnApi;
+    params.api.sizeColumnsToFit();
+
+    // Following line dymanic set height to row on content
+    params.api.resetRowHeights();
+    this.gridApi.setGridAutoHeight(true)
   }
 
-  SearchWord(keyword: HTMLInputElement){
+  SearchWord(keyword: HTMLInputElement, number_of_tweets: HTMLInputElement){
+    this.rowData = []
     let obj = {
-      'keyword': keyword.value
+      'keyword': keyword.value,
+      'number_of_tweets' : parseInt(number_of_tweets.value)
     }
-    this.homeService.populteGridData(obj).subscribe((response) => {      
-      this.rowData.push(response)
+    this.homeService.populteGridData(obj).subscribe((response) => { 
+      for (let i = 0; i< response['tweets'].length; i++) {
+        this.rowData.push({'tweets' : response['tweets'][i], 'sentiments' : response['sentiments'][i]})
+      }
       this.gridApi.setRowData(this.rowData)
     })
+  }
+
+  updateForm(event) {
+    if (event.target.value.toString().length >= 2) {
+      event.target.value = 100;
+      event.preventDefault()
+    }
   }
 
   onModelUpdated(params){
